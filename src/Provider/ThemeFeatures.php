cabd6cf74c47e15562977f51e4b76030d721da23
Dylan <dylan@superbia.com.au>
2018-06-25 13:17:09 +1000

    Enable google CDN version of jQuery.
    - Load jQuery from the google CDN
    - Fallback to local version if the CDN version doesn’t load
    - Add a DNS prefetch for the CDN domain.

--- src/Provider/ThemeFeatures.php
+++ src/Provider/ThemeFeatures.php
@@ -23,5 +23,36 @@ class ThemeFeatures implements HookProviderInterface, PluginAwareInterface {
 	/**
 	 * Register hooks.
 	 */
-	public function register_hooks() {}
+	public function register_hooks() {
+		add_action( 'after_setup_theme', array( $this, 'maybe_require_feature_mods' ), 100 );
+	}
+
+	/**
+	 * Conditionally require theme features.
+	 *
+	 * Checks for theme support before requiring the feature file
+	 *
+	 * @since 0.1.0
+	 */
+	public function maybe_require_feature_mods() {
+		foreach ( $this->get_features() as $feature ) {
+			if ( current_theme_supports( 'blackbird-' . $feature ) ) {
+				require_once $this->plugin->get_path( '/features/' . $feature . '.php' );
+			}
+		}
+	}
+
+	/**
+	 * All feature modifications.
+	 *
+	 * @since 0.1.0
+	 *
+	 * @return array An array of features modifications
+	 */
+	public static function get_features() {
+		return array(
+			'enable-cdn-jquery',
+		);
+	}
+
 }
